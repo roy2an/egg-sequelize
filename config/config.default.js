b3fa898103f3a0d774398fa72bec9e3bc6bc3a0b
Roy.Yan <roy2an@163.com>
2018-06-20 21:11:24 +0800

    多库

--- config/config.default.js
+++ config/config.default.js
@@ -8,3 +8,19 @@ exports.sequelize = {
   username: 'root',
   password: '',
 };
+
+// exports.sequelize = [{
+//   dialect: 'mysql',
+//   database: '',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root',
+//   password: '',
+// }, {
+//   dialect: 'mysql',
+//   database: '',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'root',
+//   password: '',
+// }];
