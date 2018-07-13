64570ad05dee46db51e2a59e2e831ef10952c61d
Roy.Yan <roy2an@163.com>
2018-07-13 18:17:16 +0800

    异步配置调用

--- test/fixtures/apps/model-app/app/model/tracing.js
+++ test/fixtures/apps/model-app/app/model/tracing.js
@@ -4,13 +4,13 @@ const assert = require('assert');
 
 module.exports = app => {
   const { INTEGER, STRING } = app.Sequelize;
-  const Tracing = app.dbs.ga.define('tracing', {
+  const Tracing = app.models.ga.define('tracing', {
     user_id: INTEGER,
     name: STRING(30),
   });
 
   Tracing.associate = function() {
-    assert.ok(app.dbs.ga.Tracing);
+    assert.ok(app.models.ga.Tracing);
   };
 
   return Tracing;
--- test/fixtures/apps/model-app/config/config.js
+++ test/fixtures/apps/model-app/config/config.js
@@ -1,33 +1,76 @@
 'use strict';
 
-exports.sequelize = [{
-  port: '3306',
-  host: '127.0.0.1',
-  username: 'root',
-  password: '',
-  database: 'test',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-  storage: 'db/test-foo.sqlite',
-  timezone: '+08:01',
-}, {
-  port: '3306',
-  host: '127.0.0.1',
-  username: 'root',
-  password: '',
-  database: 'ga',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-  storage: 'db/test-foo.sqlite',
-  timezone: '+08:01',
-}];
+const req = function(time) {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve(
+        [{
+          port: '3306',
+          host: '127.0.0.1',
+          username: 'root',
+          password: '',
+          database: 'test',
+          dialect: 'mysql',
+          pool: {
+            max: 5,
+            min: 0,
+            idle: 10000,
+          },
+          storage: 'db/test-foo.sqlite',
+          timezone: '+08:01',
+        }, {
+          port: '3306',
+          host: '127.0.0.1',
+          username: 'root',
+          password: '',
+          database: 'ga',
+          dialect: 'mysql',
+          pool: {
+            max: 5,
+            min: 0,
+            idle: 10000,
+          },
+          storage: 'db/test-foo.sqlite',
+          timezone: '+08:01',
+        }]
+      );
+    }, time);
+  });
+};
+
+exports.sequelize = async () => {
+  const data = await req(3000);
+  return data;
+};
+
+// exports.sequelize = [{
+//             port: '3306',
+//             host: '127.0.0.1',
+//             username: 'root',
+//             password: '',
+//             database: 'test',
+//             dialect: 'mysql',
+//             pool: {
+//               max: 5,
+//               min: 0,
+//               idle: 10000,
+//             },
+//             storage: 'db/test-foo.sqlite',
+//             timezone: '+08:01',
+//           }, {
+//             port: '3306',
+//             host: '127.0.0.1',
+//             username: 'root',
+//             password: '',
+//             database: 'ga',
+//             dialect: 'mysql',
+//             pool: {
+//               max: 5,
+//               min: 0,
+//               idle: 10000,
+//             },
+//             storage: 'db/test-foo.sqlite',
+//             timezone: '+08:01',
+//           }]
 
 exports.keys = '0jN4Fw7ZBjo4xtrLklDg4g==';
