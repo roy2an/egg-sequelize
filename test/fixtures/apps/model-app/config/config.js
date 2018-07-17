57104bc61b454aa8627c0ee066c0a0cbb5c5c270
Roy.Yan <roy2an@163.com>
2018-07-17 16:15:23 +0800

    eslint

--- test/fixtures/apps/model-app/config/config.js
+++ test/fixtures/apps/model-app/config/config.js
@@ -1,68 +1,70 @@
 'use strict';
 
-const getConfig = function(time) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
-      resolve(
-        [{
-          port: '3306',
-          host: '127.0.0.1',
-          username: 'root',
-          password: '',
-          database: 'test',
-          dialect: 'mysql',
-          pool: {
-            max: 5,
-            min: 0,
-            idle: 10000,
-          }
-        }, {
-          port: '3306',
-          host: '127.0.0.1',
-          username: 'root',
-          password: '',
-          database: 'ga',
-          dialect: 'mysql',
-          pool: {
-            max: 5,
-            min: 0,
-            idle: 10000,
-          }
-        }]
-      );
-    }, time);
-  });
-};
+// const getConfig = function(time) {
+//   return new Promise(function(resolve) {
+//     setTimeout(function() {
+//       resolve(
+//         [{
+//           port: '3306',
+//           host: '127.0.0.1',
+//           username: 'root',
+//           password: '',
+//           database: 'test',
+//           dialect: 'mysql',
+//           pool: {
+//             max: 5,
+//             min: 0,
+//             idle: 10000,
+//           }
+//         }, {
+//           port: '3306',
+//           host: '127.0.0.1',
+//           username: 'root',
+//           password: '',
+//           database: 'ga',
+//           dialect: 'mysql',
+//           pool: {
+//             max: 5,
+//             min: 0,
+//             idle: 10000,
+//           }
+//         }]
+//       );
+//     }, time);
+//   });
+// };
 
-exports.sequelize = async () => {
-  const data = await getConfig(3000);
-  return data;
-};
+// exports.sequelize = async () => {
+//   const data = await getConfig(3000);
+//   return data;
+// };
 
-// exports.sequelize = [{
-//             port: '3306',
-//             host: '127.0.0.1',
-//             username: 'root',
-//             password: '',
-//             database: 'test',
-//             dialect: 'mysql',
-//             pool: {
-//               max: 5,
-//               min: 0,
-//               idle: 10000,
-//             },
-//           }, {
-//             port: '3306',
-//             host: '127.0.0.1',
-//             username: 'root',
-//             password: '',
-//             database: 'ga',
-//             dialect: 'mysql',
-//             pool: {
-//               max: 5,
-//               min: 0,
-//               idle: 10000,
-//             },
-//           }]
+exports.sequelize = [{
+  port: '3306',
+  host: '127.0.0.1',
+  username: 'root',
+  password: '',
+  database: 'test',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  storage: 'db/test-foo.sqlite',
+  timezone: '+08:01',
+}, {
+  port: '3306',
+  host: '127.0.0.1',
+  username: 'root',
+  password: '',
+  database: 'ga',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+}];
 
 exports.keys = '0jN4Fw7ZBjo4xtrLklDg4g==';
